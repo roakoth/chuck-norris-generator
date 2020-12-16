392cc53507f8608f3e6e1cd6adbd70f5d7a921a3
roakoth <roseakoth2015@gmail.com>
2020-12-15 21:46:15 -0800

    made changes to Joke component

--- src/Joke.js
+++ src/Joke.js
@@ -6,13 +6,13 @@ const Joke = (props) => {
   React.useEffect(() => {
     fetch(`https://api.chucknorris.io/jokes/random?category=${props.category}`)
       .then((response) => response.json())
-      .then((jokeResponse) => setJoke(jokeResponse));
+      .then((jokeResponse) => setJoke(jokeResponse.value));
   }, [props.category]);
 
   if (!joke) {
     <p> Please select a category</p>;
   }
-  return <p> {joke.value} </p>;
+  return <p> {joke} </p>;
 };
 
 export default Joke;
